0e75c22cccab659108bb1a0e36453cc5f57aa7ac
gstinoco <gstinoco@gmail.com>
2023-03-02 20:53:20 -0600

    Mesh formulation improved.

--- run_meshes.py
+++ run_meshes.py
@@ -98,8 +98,8 @@ for reg in regions:
         y  = mat['y']
 
         # Poisson 2D computed in a logically rectangular mesh
-        u_ap, u_ex = Diffusion_2D.Mesh(x, y, fDIF, v, t)
-        er = Errors.Mesh(x, y, u_ap, u_ex, implicit = True)
+        u_ap, u_ex = Diffusion_2D.Mesh(x, y, fDIF, v, t, implicit = True)
+        er = Errors.Mesh(x, y, u_ap, u_ex)
         print(regi, 'size' ,mesh, '. Implicit scheme: ', er.max())
         Graph.Error_sav(er, nom)
         Graph.Mesh_Transient_sav(x, y, u_ap, u_ex, nov)
